94cd7195a742c6f440f22c9fdfb8cbe2802e86e3
ann-kathrein <59734628+ann-kathrein@users.noreply.github.com>
2020-03-06 08:34:01 +0100

    Formular
    Added Textarea fpr message and JS

--- sharedscript.js
+++ sharedscript.js
@@ -1,3 +1,6 @@
+'use strict';
+
+/*
 var backdrop = document.querySelector('#backdrop');
 var toggleButton = document.querySelector('.toggle-button ');
 var mobileNav = document.querySelector('.mobileNav ');
@@ -8,31 +11,28 @@ function closeMobileNav() {
 }
 
 /*Open Navigation Menu and Backdrop when clicking on Hamburger Menu*/
-toggleButton.addEventListener('click', function() {
+/*toggleButton.addEventListener('click', function() {
 	mobileNav.style.display = 'block';
 	backdrop.style.display = 'block';
-});
+})
+*/
 
 /*Close Mobile Navigation and Backdrop when clicking on Backdrop or Mobile Nav*/
-backdrop.addEventListener('click', closeMobileNav);
-mobileNav.addEventListener('click', closeMobileNav);
-
-
-
+/*backdrop.addEventListener('click', closeMobileNav);
+mobileNav.addEventListener('click', closeMobileNav); */
 
 /*FORMULAR*/
-const firstName = document.getElementbyID('firstName');
-const lastName = document.getElementbyID('lastName');
-const mailAdressInput = document.getElementbyID('mailAdress');
-const message = document.getElementbyID('message');
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const mailAdressInput = document.getElementById('mailAdress');
+const message = document.getElementById('message');
+const submitBtn = document.getElementById('submitBtn');
 
-let mailAdressClean = mailAdressInput.trim().toLowerCase();
+/*let mailAdressClean = mailAdressInput.trim().toLowerCase();*/
 
-function createDocument() { 
-    let create = document.getElementByID('firstName', 'lastName', 'mailAdress', 'message');
+function submitForm() {
+	event.preventDefault();
+	alert('Name: ${firstName}${lastName}, eMail: ${mailAdressClean}, Message: ${message}');
 }
 
-
-create.addEventListener('click', 'createFile', function () ){ 
-
-}
+submitBtn.addEventListener('click', submitForm);
