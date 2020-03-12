71a8cc0660cb75b624709721c72dd5f967863013
ann-kathrein <59734628+ann-kathrein@users.noreply.github.com>
2020-03-12 18:43:08 +0100

    Mobile Nav display and display none

--- sharedscript.js
+++ sharedscript.js
@@ -1,31 +1,19 @@
 'use strict';
 
-const mobileNav = document.querySelector('.smallScreen');
-const menuButton = document.querySelector('.checkbox');
-const menuButton = document.querySelector('.checkbox');
+// ######## Nav mobile ###########
 
-/*
-var backdrop = document.querySelector('#backdrop');
-var toggleButton = document.querySelector('.toggle-button ');
-var mobileNav = document.querySelector('.mobileNav ');
+document.getElementById('hamburger').addEventListener('click', navStatus);
 
-function closeMobileNav() {
-	mobileNav.style.display = 'none';
-	backdrop.style.display = 'none';
+function navStatus() {
+  let menuElement = document.querySelector('.menu').classList;
+  if (menuElement.contains('hamburger-active')) {
+    menuElement.remove('hamburger-active');
+  } else {
+    menuElement.add('hamburger-active');
+  }
 }
 
-/*Open Navigation Menu and Backdrop when clicking on Hamburger Menu*/
-/*toggleButton.addEventListener('click', function() {
-	mobileNav.style.display = 'block';
-	backdrop.style.display = 'block';
-})
-*/
-
-/*Close Mobile Navigation and Backdrop when clicking on Backdrop or Mobile Nav*/
-/*backdrop.addEventListener('click', closeMobileNav);
-mobileNav.addEventListener('click', closeMobileNav); */
-
-/*FORMULAR*/
+// ####### FORMULAR #########
 const firstNameInput = document.getElementById('firstName');
 const lastNameInput = document.getElementById('lastName');
 const mailAdressInput = document.getElementById('mailAdress');
@@ -35,10 +23,10 @@ const submitBtn = document.getElementById('submitBtn');
 //let mailAdressClean = mailAdressInput.trim().toLowerCase();
 
 function submitForm() {
-	event.preventDefault();
-	alert(
-		`Name: ${firstNameInput.value} ${lastNameInput.value}, eMail: ${mailAdressInput.value}, Message: ${messageInput.value}`
-	);
+  event.preventDefault();
+  alert(
+    `Name: ${firstNameInput.value} ${lastNameInput.value}, eMail: ${mailAdressInput.value}, Message: ${messageInput.value}`
+  );
 }
 
 submitBtn.addEventListener('click', submitForm);
